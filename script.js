caa957c5422f311c96b53a58f3af631b1aa29144
SnakeOS <160025896+SnakeOS@users.noreply.github.com>
2024-02-14 18:42:52 +0545

    new file added

---
+++ script.js
@@ -0,0 +1,87 @@
+<div class="containe">
+  <footer class="footer">
+    <div class="aside">
+      <img
+        class="logo"
+        src="image/Picture4.png "
+        alt="Collage Logo"
+        height="50"
+        width="100"
+      />
+      <div class="text" style="text-align: justify">
+        <p style="width: 500px; color: gray">
+          NCMT has interdisciplinary teams with flat hierarchies and
+          opportunities to learn and grow. High individual responsibility and
+          possibility to let own ideas thrive. Our internal processes and
+          software enable to work fully remote in distributed teams but keeping
+          closely in touch in day to day work.
+        </p>
+      </div>
+    </div>
+    <div class="aside">
+      <h2 style="color: white; border-bottom: 2px solid blueviolet">
+        Quick Links
+      </h2>
+      <a href="">Home</a>
+      <a href="">About Us</a>
+      <a href="">Why NCMT</a>
+      <a href="">Request Call Back</a>
+      <a href="">Contact Us</a>
+    </div>
+    <div class="aside">
+      <h2 style="color: white; border-bottom: 2px solid blueviolet">
+        Our Program
+      </h2>
+      <a href="">BCA</a>
+      <a href="">BSC.Csit</a>
+    </div>
+    <div class="aside">
+      <h2 style="color: white; border-bottom: 2px solid blueviolet">
+        Get In Touch
+      </h2>
+      <a href="">081-532883</a>
+      <a href="">+9779848022391</a>
+      <a href="">Sunday-Friday, 6 AM to 12 PM</a>
+      <div style="display: flex; flex-direction: row">
+        <p>
+          <a href="https://www.youtube.com">
+            <img
+              src="image/facebook.png"
+              style="height: 20px; width: 20px; margin: 5px"
+              alt="facebook Logo"
+            />
+          </a>
+        </p>
+        <p>
+          <a href="https://www.youtube.com">
+            <img
+              src="image/youtube.png"
+              style="height: 20px; width: 20px; margin: 5px"
+              alt="YouTube Logo"
+            />
+          </a>
+        </p>
+      </div>
+    </div>
+  </footer>
+  <div class="base">
+    <div class="l">
+      <p>
+        &copy; copyright
+        <a href="" style="display: inline; color: white">
+          NCMT
+        </a>
+        . All the reserve.
+      </p>
+    </div>
+    <div class="r">
+      <p>
+        Developed by
+        <a href="" style="display: inline; color: white">
+          NCMT
+        </a>
+        .
+      </p>
+    </div>
+  </div>
+</div>;
